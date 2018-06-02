37993b9cdc2426d438f6587c5ad8fc0bbac9b8a2
Mikhail Pontus <m.pontus@gmail.com>
2018-06-02 18:44:49 +0300

    Use joke generator deployed as google cloud endpoint

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,19 +10,19 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.jokeactivity.JokeActivity;
-import com.example.jokes.JokeGenerator;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private JokeGenerator jokeGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        jokeGenerator = new JokeGenerator();
     }
 
 
@@ -47,12 +49,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokeActivity.class);
+        EndpointsAsyncTask task = new EndpointsAsyncTask(this);
 
-        intent.putExtra(JokeActivity.EXTRA_TEXT, jokeGenerator.getJoke());
-
-        startActivity(intent);
+        task.execute();
     }
 
 
+    static final class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+        private final Context context;
+
+        public EndpointsAsyncTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            if (myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("https://udacity-build-it-bigger-74739.appspot.com/_ah/api/");
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(context, JokeActivity.class);
+
+            intent.putExtra(JokeActivity.EXTRA_TEXT, result);
+
+            context.startActivity(intent);
+        }
+    }
 }
