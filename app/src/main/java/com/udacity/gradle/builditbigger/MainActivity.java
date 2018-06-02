2ab0a0879148b3d510fbe8b85af34c47159c5b58
Mikhail Pontus <m.pontus@gmail.com>
2018-06-02 17:19:46 +0300

    Implement Joke Activity as a library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+import com.example.jokeactivity.JokeActivity;
 import com.example.jokes.JokeGenerator;
 
 
@@ -46,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, jokeGenerator.getJoke(), Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+
+        intent.putExtra(JokeActivity.EXTRA_TEXT, jokeGenerator.getJoke());
+
+        startActivity(intent);
     }
 
 
---
+++ jokeactivity/src/main/java/com/example/jokeactivity/JokeActivity.java
@@ -0,0 +1,43 @@
+package com.example.jokeactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "EXTRA_TEXT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(EXTRA_TEXT);
+        TextView contentView = (TextView) findViewById(R.id.content);
+
+        contentView.setText(text);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
